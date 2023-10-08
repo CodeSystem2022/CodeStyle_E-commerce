49c26454c29c81e779d3f83bbdacea9872240d2c
Adriana Soledad Da Silva <adrianasilva523@gmail.com>
2023-10-08 20:59:47 -0300

    Agregamos el crud

---
+++ crud/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Pedidos, Clientes, DetallePedido, InformacionNutricional, Ingredientes, Producto, Usuarios
+
+admin.site.register(Pedidos)
+admin.site.register(Clientes)
+admin.site.register(DetallePedido)
+admin.site.register(InformacionNutricional)
+admin.site.register(Ingredientes)
+admin.site.register(Producto)
+admin.site.register(Usuarios)
+
