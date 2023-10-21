1f3e5b2e44cde578d5747b5895dddec68ebfe162
Florencia Micaela Oviedo <flor.oviedo2010@gmail.com>
2023-10-21 19:54:16 -0300

    creando env

--- NutriStyle/settings.py
+++ NutriStyle/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'NutriStyle',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': 5432
     }
